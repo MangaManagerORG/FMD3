3dc029c1afea9e8713f6aa29cc0affcb2b413c42
ThePromidius <thepromidiusyt@gmail.com>
2024-01-17 18:24:38 +0100

    implementation of a test ui

---
+++ src/FMD3/DumbUI/__init__.py
@@ -0,0 +1,56 @@
+import io
+import tkinter
+from urllib import request
+from PIL import Image, ImageTk
+from FMD3.Extensions import extesion_factory, IExtension, list_extension, get_extension
+from FMD3.Models.MangaInfo import MangaInfo
+
+
+def load_data():
+    url = var.get()
+    sel_ext = clicked.get()
+
+    ext:IExtension = get_extension(sel_ext)
+    mi:MangaInfo = ext.on_get_info(url)
+
+    res = f"""
+    Series name: {mi.title}
+    Num of chapters: {len(mi.chapters)}
+    genres: {mi.genres}
+    authors: {mi.authors}
+    artists: {mi.artists}
+    description: {mi.description}
+    """
+
+    raw_data = request.urlopen(mi.cover_url).read()
+    im = Image.open(io.BytesIO(raw_data))
+    image = ImageTk.PhotoImage(im)
+    image_lbl = tkinter.Label(root, image=image)
+    image_lbl.image = image
+
+    image_lbl.pack()
+
+    result.set(res)
+
+root = tkinter.Tk()
+
+extensions = list_extension()
+
+clicked = tkinter.StringVar()
+drop = tkinter.OptionMenu(root, clicked, *extensions)
+drop.pack()
+result = tkinter.StringVar()
+tkinter.Label(root,textvariable=clicked).pack()
+var = tkinter.StringVar()
+tkinter.Entry(root,textvariable=var).pack()
+
+tkinter.Button(root,command=load_data,text="Load_data").pack()
+
+
+
+tkinter.Label(root,textvariable=result,justify="left").pack()
+
+
+
+
+root.mainloop()

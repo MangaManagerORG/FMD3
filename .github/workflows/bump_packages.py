d471a9af5500089df00534a599a24ef4a471d424
ThePromidius <thepromidiusyt@gmail.com>
2024-02-12 10:40:01 +0100

    wrong env in bump_packages.py

--- .github/workflows/bump_packages.py
+++ .github/workflows/bump_packages.py
@@ -5,5 +5,6 @@ from versioning_utils import bump_packages_if_modified
 if __name__ == '__main__':
     src_path = 'src'
     packages = ['FMD3', 'FMD3_API', 'FMD3_Tkinter']
-    bump_level = os.getenv('BUMP_LEVEL', 'patch')
+    bump_level = os.getenv('BUMP_TYPE', 'patch')
+    print('Bump level:', bump_level)
     bump_packages_if_modified(src_folder=src_path, packages=packages, bump_level=bump_level)

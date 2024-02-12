e453832046ffa86802b4a8bd99c4e99261660597
ThePromidius <thepromidiusyt@gmail.com>
2024-02-12 13:08:42 +0100

    fixed run config

--- src/FMD3_API/__main__.py
+++ src/FMD3_API/__main__.py
@@ -1,13 +1,5 @@
 import uvicorn
 from FMD3_API.app import app
-from __version__ import __version__
-
-
-@app.get("/")
-async def root():
-    return {"message": "Successfully running FMD3 API",
-            "version": __version__}
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- src/FMD3_API/app/__init__.py
+++ src/FMD3_API/app/__init__.py
@@ -1,8 +1,13 @@
 
 from fastapi import FastAPI
 from .routers import series, sources, chapters, settings
-app = FastAPI()
+from ..__version__ import __version__
 
+app = FastAPI()
+@app.get("/")
+async def root():
+    return {"message": "Successfully running FMD3 API",
+            "version": __version__}
 app.include_router(series.router)#, prefix="/series", tags=["series"])
 app.include_router(sources.router)
 app.include_router(chapters.router)

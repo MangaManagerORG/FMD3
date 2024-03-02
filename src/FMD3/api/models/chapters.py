103450380c90e8ca91c8c99b064a935f7bd53985
ThePromidius <thepromidiusyt@gmail.com>
2024-03-02 19:16:16 +0100

    fixed action missing artifacts. no longer bundled but single exe

--- src/FMD3/api/models/chapters.py
+++ src/FMD3/api/models/chapters.py
@@ -7,22 +7,26 @@ from FMD3.models.ddl_chapter_status import DLDChaptersStatus
 
 
 class ChapterResponse(BaseModel):
-    chapter_id:str
-    series_id:str
-    volume:float|None
-    number:int
-    title:str|None
-    status:DLDChaptersStatus
-    path:str
+    chapter_id: str
+    series_id: str
+    volume: float | None
+    number: int
+    title: str | None
+    status: DLDChaptersStatus
+    path: str
     added_at: datetime.datetime | None
-    downloaded_at:datetime.datetime | None
+    downloaded_at: datetime.datetime | None
+
+
 class SourceChapterResponse(BaseModel):
-    chapter_id:str
-    volume:float|None
-    number:float
-    title:str|None
-    pages:int
-    scanlator:str|None
+    chapter_id: str
+    volume: float | None
+    number: float
+    title: str | None
+    pages: int
+    scanlator: str | None
+
+
 # {
 #             "chapter_id": chapter.chapter_id,
 #             "series_id": chapter.series_id,
@@ -35,9 +39,9 @@ class SourceChapterResponse(BaseModel):
 #         }
 
 class DownloadChapterForm(BaseModel):
-    output_path:str
-    series_id:str
-    source_id:str
-    fav_series:bool
-    enable_series:bool
-    chapter_ids:list[str] | Literal["all"]
+    output_path: str
+    series_id: str
+    source_id: str
+    fav_series: bool
+    enable_series: bool
+    chapter_ids: list[str] | Literal["all"]
